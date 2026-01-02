2adc623a85f1674fb7b6425a4db2e0045b0852ad
hababdu <habibullox000@icloud.com>
2026-01-02 17:24:17 +0500

    Fix file path in server.js for static file serving to ensure correct loading of index.html.

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 // ==================== STATIC FAYLLARNI SERVIS QILISH ====================
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
 app.get('/main.js', (req, res) => {
